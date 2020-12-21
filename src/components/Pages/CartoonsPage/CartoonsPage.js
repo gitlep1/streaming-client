1c6464438555f25a403fe6926a6d08fbee6e341f
ashivs2500 <antonioshivers2500@yahoo.com>
2020-12-21 10:05:13 -0500

    styled the cartoons page

--- src/components/Pages/CartoonsPage/CartoonsPage.js
+++ src/components/Pages/CartoonsPage/CartoonsPage.js
@@ -1,13 +1,62 @@
 import React from 'react'
-// import { Link } from 'react-router-dom'
-
-// import edEddEddyImage from '../../../ImagesVideos/Ed-Edd-N-Eddy-crazy-faces.png'
+import { HashLink as Link } from 'react-router-hash-link'
 
 import './CartoonsPage.scss'
 
 const CartoonsPage = () => (
   <div>
-
+    <Link to="#sectionA" className="sectionALink">A</Link>
+    <Link to="#sectionB" className="sectionBLink">B</Link>
+    <Link to="#sectionC" className="sectionCLink">C</Link>
+    <Link to="#sectionD" className="sectionDLink">D</Link>
+    <Link to="#sectionE" className="sectionELink">E</Link>
+    <Link to="#sectionF" className="sectionFLink">F</Link>
+    <Link to="#sectionG" className="sectionGLink">G</Link>
+    <Link to="#sectionH" className="sectionHLink">H</Link>
+    <Link to="#sectionI" className="sectionILink">I</Link>
+    <Link to="#sectionJ" className="sectionJLink">J</Link>
+    <Link to="#sectionK" className="sectionKLink">K</Link>
+    <Link to="#sectionL" className="sectionLLink">L</Link>
+    <Link to="#sectionM" className="sectionMLink">M</Link>
+    <Link to="#sectionN" className="sectionNLink">N</Link>
+    <Link to="#sectionO" className="sectionOLink">O</Link>
+    <Link to="#sectionP" className="sectionPLink">P</Link>
+    <Link to="#sectionQ" className="sectionQLink">Q</Link>
+    <Link to="#sectionR" className="sectionRLink">R</Link>
+    <Link to="#sectionS" className="sectionSLink">S</Link>
+    <Link to="#sectionT" className="sectionTLink">T</Link>
+    <Link to="#sectionU" className="sectionULink">U</Link>
+    <Link to="#sectionV" className="sectionVLink">V</Link>
+    <Link to="#sectionW" className="sectionWLink">W</Link>
+    <Link to="#sectionX" className="sectionXLink">X</Link>
+    <Link to="#sectionY" className="sectionYLink">Y</Link>
+    <Link to="#sectionZ" className="sectionZLink">Z</Link>
+    <h2 id="sectionA">A</h2>
+    <h2 id="sectionB">B</h2>
+    <h2 id="sectionC">C</h2>
+    <h2 id="sectionD">D</h2>
+    <h2 id="sectionE">E</h2>
+    <h2 id="sectionF">F</h2>
+    <h2 id="sectionG">G</h2>
+    <h2 id="sectionH">H</h2>
+    <h2 id="sectionI">I</h2>
+    <h2 id="sectionJ">J</h2>
+    <h2 id="sectionK">K</h2>
+    <h2 id="sectionL">L</h2>
+    <h2 id="sectionM">M</h2>
+    <h2 id="sectionN">N</h2>
+    <h2 id="sectionO">O</h2>
+    <h2 id="sectionP">P</h2>
+    <h2 id="sectionQ">Q</h2>
+    <h2 id="sectionR">R</h2>
+    <h2 id="sectionS">S</h2>
+    <h2 id="sectionT">T</h2>
+    <h2 id="sectionU">U</h2>
+    <h2 id="sectionV">V</h2>
+    <h2 id="sectionW">W</h2>
+    <h2 id="sectionX">X</h2>
+    <h2 id="sectionY">Y</h2>
+    <h2 id="sectionZ">Z</h2>
   </div>
 )
 
